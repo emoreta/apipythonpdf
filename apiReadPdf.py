6237221ccfc811413bfdbe9e3abe560d4227bf06
Guaba Studio <72534443+emoreta@users.noreply.github.com>
2025-02-07 20:49:31 -0500

    Update apiReadPdf.py

--- apiReadPdf.py
+++ apiReadPdf.py
@@ -46,4 +46,4 @@ async def read_pdf(base64_pdf: str = Form(...)):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Usa el puerto de la variable de entorno o 8000 por defecto
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("apiReadPdf:app", host="0.0.0.0", port=port)
