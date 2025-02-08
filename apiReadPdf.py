df80ba0470a1a3ea6e80d0ac0bb2e7a2139faab0
Guaba Studio <72534443+emoreta@users.noreply.github.com>
2025-02-07 20:58:53 -0500

    Update apiReadPdf.py

--- apiReadPdf.py
+++ apiReadPdf.py
@@ -44,6 +44,6 @@ async def read_pdf(base64_pdf: str = Form(...)):
     except Exception as e:
         return PDFResponse(pages=0, text="", success=False, message=f"Error decodificando el archivo: {str(e)}")
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Usa el puerto de la variable de entorno o 8000 por defecto
-    uvicorn.run("apiReadPdf:app", host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 8000))  # Usa el puerto de la variable de entorno o 8000 por defecto
+#    uvicorn.run("apiReadPdf:app", host="0.0.0.0", port=port)
